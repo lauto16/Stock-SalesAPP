80941a11d6a47094ee85545d7fae93aca0e5a0ba
David Walter Koch <402474@sistemas.frc.utn.edu.ar>
2025-07-24 19:50:06 -0300

    sideBar and components

--- Frontend/TiendaClick/src/App.jsx
+++ Frontend/TiendaClick/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Dashboard from './components/Dashboard.jsx'
+import Dashboard from './components/dashboard/Dashboard.jsx'
 function App() {
 
   return (
---
+++ Frontend/TiendaClick/src/components/sideNav/SideBar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import NavLink from './NavLink.jsx'
+
+function SideBar() {
+    return (
+
+        <aside className="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+            {/* ACA IRIA EL NOMBRE DE LA APP desde otro component*/}
+
+            <div className="sidebar-brand">
+                <a href="../index.html" className="brand-link">
+                    <img src="../assets/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image opacity-75 shadow" />
+                    <span className="brand-text fw-light">TiendaClick</span>
+                </a>
+            </div>
+
+
+            <div className="sidebar-wrapper">
+                <nav className="mt-2">
+                    <ul className="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="navigation" aria-label="Main navigation" data-accordion="false" id="navigation">
+
+
+                        <NavLink name={"Panel Administrativo"}></NavLink>
+                        <NavLink name={"Estadísticas"}></NavLink>
+                        <NavLink name={"Ventas"}></NavLink>
+                        <NavLink name={"Inventario"}></NavLink>
+                        {/* Titulo */}
+                        <li class="nav-header">DESCARGAR DOCUMENTOS</li>
+                        <NavLink name={"Informe de Ventas"}></NavLink>
+
+                    </ul>
+                </nav>
+            </div>
+        </aside>
+    );
+}
+export default SideBar; 
