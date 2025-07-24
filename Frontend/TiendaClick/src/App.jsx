7326041287f98649704e5802cb4b0b9fccf7c7fc
Lautaro Martino <73772193+lauto16@users.noreply.github.com>
2025-07-24 18:53:33 -0300

    add inventory to routes

--- Frontend/TiendaClick/src/App.jsx
+++ Frontend/TiendaClick/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard.jsx'
+import Inventory from './components/Inventory.jsx'
+
 function App() {
 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/inventory/" element={<Inventory />} />
       </Routes>
     </Router>
 
