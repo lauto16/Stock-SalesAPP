6db51dcdafa8b5aeba5c47e7810ab2f8f24057bb
lauto16 <lautymartino658@gmail.com>
2025-12-04 23:07:50 -0300

    add authguard component to check if user is logged, otherwise redirect them to logibn}

--- Frontend/TiendaClick/src/context/UserContext.jsx
+++ Frontend/TiendaClick/src/context/UserContext.jsx
@@ -52,7 +52,7 @@ export const UserProvider = ({ children }) => {
     };
 
     return (
-        <UserContext.Provider value={{ user, login, logout }}>
+        <UserContext.Provider value={{ user, login, setUser, logout }}>
             {children}
         </UserContext.Provider>
     );
