d66afe92b1402aeaae41de1b30a31b6d33871b5d
lauto16 <lautymartino658@gmail.com>
2025-12-09 23:42:41 -0300

    backend for sales at 98% completion

--- Backend/SalesAPI/models.py
+++ Backend/SalesAPI/models.py
@@ -4,22 +4,20 @@ from InventoryAPI.models import Product
 
 
 class Sale(models.Model):
-    applied_discount_percentage = models.FloatField(default=0)
-    discount_reason = models.CharField(max_length=200, blank=True)
+    applied_charge_percentage = models.FloatField(default=0)
+    charge_reason = models.CharField(max_length=200, blank=True)
     initial_price = models.FloatField(default=0)
-    # Total price when applied 'applied_discount_percentage' and 'tax_percentage'
+    # Total price when applied 'applied_charge_percentage'
     total_price = models.FloatField(default=0)
-    tax_percentage = models.FloatField(default=21)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
 
     def update_totals(self):
         initial = sum(item.subtotal for item in self.items.all())
-        discounted = initial * (1 - self.applied_discount_percentage / 100)
-        taxed = discounted * (1 + self.tax_percentage / 100)
+        charged = initial * (1 - self.applied_charge_percentage / 100)
 
         self.initial_price = initial
-        self.total_price = taxed
+        self.total_price = charged
         self.save()
 
     def finalize_sale(self, user=None):
@@ -40,9 +38,9 @@ class SaleItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveIntegerField()
     unit_price = models.FloatField()
-    discount_percentage = models.FloatField(default=0)
+    charge_percentage = models.FloatField(default=0)
 
     @property
     def subtotal(self):
         """Returns the subtotal of an SaleItem"""
-        return self.quantity * self.unit_price * (1 - self.discount_percentage / 100)
+        return self.quantity * self.unit_price * (1 - self.charge_percentage / 100)
