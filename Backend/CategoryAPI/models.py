3fda5f2f707537659406c8657ca07c728bc3eac2
lauto16 <lautymartino658@gmail.com>
2025-12-14 20:04:58 -0300

    change so Category's primary key is id

--- Backend/CategoryAPI/models.py
+++ Backend/CategoryAPI/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=200, primary_key=True)
+    name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True, default='-')
