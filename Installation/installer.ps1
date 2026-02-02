68e4063bf7a7e381881e3448c8db3f830c488806
Lautaro Martino <73772193+lauto16@users.noreply.github.com>
2026-02-02 08:34:03 -0300

    Create PERSONAL_IDENTIFIER.json and install virtualenv
    Add script to create PERSONAL_IDENTIFIER.json and install virtualenv.

--- Installation/installer.ps1
+++ Installation/installer.ps1
@@ -56,7 +56,21 @@ Set-Location $TargetDir
 # ============================
 # Backend
 # ============================
+
 Set-Location (Join-Path $TargetDir $BackendPath)
+
+# ============================
+# Create PERSONAL_IDENTIFIER.json
+# ============================
+$PersonalIdentifierPath = Join-Path (Get-Location) "PERSONAL_IDENTIFIER.json"
+
+Log "Creando PERSONAL_IDENTIFIER.json..."
+
+@'
+{
+  "key": ""
+}
+'@ | Set-Content -Path $PersonalIdentifierPath -Encoding UTF8
 
 Log "Instalando virtualenv..."
 & $PythonCmd -m pip install virtualenv
