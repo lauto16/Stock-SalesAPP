f6afba977793b57de15fd9110b0863021fc66caa
lauto16 <lautymartino658@gmail.com>
2026-03-24 21:52:40 -0300

    key tester and keys

---
+++ Backend/Activation/key_tester.py
@@ -0,0 +1,31 @@
+import hashlib
+import json
+import os
+
+
+def encrypt_string(hash_string):
+    sha_signature = hashlib.sha256(hash_string.encode()).hexdigest()
+    return sha_signature
+
+
+def test_key(key: str) -> bool:
+    """
+    Test if a given key is valid
+    """
+    FILE_NAME = "keys.json"
+    file_path = os.path.join(os.getcwd(), FILE_NAME)
+
+    with open(file_path, "r") as f:
+        data = json.load(f)
+
+    keys = data["keys"]
+
+    for valid_key in keys:
+        if valid_key == encrypt_string(key):
+            return True
+
+    return False
+
+
+if __name__ == "__main__":
+    print(test_key("tiendaclick_activate_9876578736493222135633745"))
