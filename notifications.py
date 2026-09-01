5e8751439805028a0cbe4a2ae95f468aa7cb3af4
Koch73 <144285829+Koch73@users.noreply.github.com>
2026-09-01 20:19:30 -0300

    Uncomment notification execution logic

--- notifications.py
+++ notifications.py
@@ -8,13 +8,13 @@ import time
 
 def run_notifications():
     print("Ejecutando notificaciones...")
-#    try:
-#        delete_old_products_notifications(months=1)
-#        create_product_expiration_notifications(limit=5)
-#        create_best_sellers_products_low_stock_notification(stock_limit=5)
-#        print("Notificaciones generadas correctamente.")
-#    except Exception as e:
-#        print("Error ejecutando notificaciones:", e)
+    try:
+        delete_old_products_notifications(months=1)
+        create_product_expiration_notifications(limit=5)
+        create_best_sellers_products_low_stock_notification(stock_limit=5)
+        print("Notificaciones generadas correctamente.")
+    except Exception as e:
+        print("Error ejecutando notificaciones:", e)
 
 
 def notification_scheduler(interval_seconds=3600):
